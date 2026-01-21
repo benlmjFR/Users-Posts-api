bf66df1311fc4cfea68120f59b74bbcf90658ac0
BENOITHAVETlmj <myungjae84@gmail.com>
2026-01-22 00:48:08 +0100

    disable prisma connect temporarily

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,20 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient {}
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
-}
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleDestroy
+// {
+//   // async onModuleInit() {
+//   //   await this.$connect();
+//   // }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
