f89ea42f73791b3516b576b5b28bac57090b2a50
BENOITHAVETlmj <myungjae84@gmail.com>
2026-01-22 01:09:21 +0100

    disable prisma connect temporarily

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.PORT) || 3000;
 
   await app.listen(port, '0.0.0.0');
 }
