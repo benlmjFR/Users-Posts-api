8bc21593ce460a1f3d2dac9370316fb353f18ab6
BENOITHAVETlmj <myungjae84@gmail.com>
2026-01-23 16:46:43 +0100

    gards for users routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,15 +20,14 @@ import { Roles } from '../common/roles/role.decorator';
 import { Role } from '../common/enums/role.enum';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
-
-// Protect this route with JWT authentication
-@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   /**
    * READ /users/profile
    */
+  // Protect this route with JWT authentication
+  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Req() req: Request) {
     return req.user;
@@ -37,8 +36,6 @@ export class UsersController {
   /**
    * GET /users?page=1&limit=10
    */
-  @UseGuards(RolesGuard)
-  @Roles(Role.ADMIN, Role.SUPER_ADMIN)
   @Get()
   getAllUsers(@Query('page') page = 1, @Query('limit') limit = 10) {
     return this.usersService.getAllUsers(+page, +limit);
@@ -47,8 +44,6 @@ export class UsersController {
   /**
    * GET /users/:id
    */
-  @UseGuards(RolesGuard)
-  @Roles(Role.ADMIN, Role.SUPER_ADMIN)
   @Get(':id')
   getUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.getUser(id);
@@ -58,7 +53,7 @@ export class UsersController {
    * PATCH /users/:id/role
    */
   @Patch(':id/role')
-  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.SUPER_ADMIN)
   updateRole(
     @Param('id', ParseIntPipe) id: number,
@@ -71,8 +66,6 @@ export class UsersController {
    * PATCH /users/:id
    */
   @Patch(':id')
-  @UseGuards(RolesGuard)
-  @Roles(Role.ADMIN, Role.SUPER_ADMIN)
   updateUser(
     @Param('id', ParseIntPipe) id: number,
     @Body() data: UpdateUserDto,
@@ -84,8 +77,6 @@ export class UsersController {
    * DELETE /users/:id
    */
   @Delete(':id')
-  @UseGuards(RolesGuard)
-  @Roles(Role.SUPER_ADMIN)
   deleteUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.deleteUser(id);
   }
